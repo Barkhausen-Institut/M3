f98aeb2e03315ad2d5239144752fd5a66e1fda96
Oliver Portee <oliver.portee@gmail.com>
2022-12-02 13:16:47 +0100

    remove old start_linux_async function

--- src/kernel/src/tiles/actmng.rs
+++ src/kernel/src/tiles/actmng.rs
@@ -272,24 +272,6 @@ impl ActivityMng {
         act.start_app_async(None)
     }
 
-    pub fn start_linux_async() -> Result<Rc<Activity>, Error> {
-        // create dummy activity on tile 1
-        let tile = tilemng::tilemux(1).tile().clone();
-        let kmem = KMemObject::new(args::get().kmem - cfg::FIXED_KMEM);
-        let act = Self::create_activity_async(
-            "linux",
-            tile,
-            tcu::FIRST_USER_EP,
-            kmem,
-            ActivityFlags::IS_LINUX,
-        )
-        .expect("Unable to create Activity for linux");
-
-        // TODO: actually start linux from here
-
-        Ok(act)
-    }
-
     pub fn remove_activity_async(id: tcu::ActId) {
         let mut actmng = INST.borrow_mut();
         // Replace item at position
